35f4b01f99f43cce9a4df7cfe0c6b64ab805cf52
Budi-aji <56950817+Budi-aji@users.noreply.github.com>
2023-02-06 10:29:39 +0700

    Fixing Router and Reinstall node_modules

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import Hero from './Components/Hero/Hero'
 import Portfolio from './Components/Portfolio/Portfolio'
 import Experiences from './Components/Experiences/Experiences'
 import Footer from './Components/Footer/Footer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
         <Header />
         <Hero />
         <Portfolio />
         <Experiences />
         <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
---
+++ src/data.js
@@ -0,0 +1,26 @@
+import {SiOpenaigym} from 'react-icons/si'
+
+export const links = [
+    {
+        name: "Home",
+        path: '/'
+    },
+    {
+        name: "About",
+        path: '/about'
+    },
+    {
+        name: "Project",
+        path: '/projects'
+    },
+    {
+        name: "Get In touch!",
+        path: '/contacts'
+    }
+]
+
+export const experiences = [
+    {
+        
+    }
+]
